6c7c8ab1b8e906946bc249a95603c63b6b338824
tyzrex <sulavbaral58@gmail.com>
2024-05-05 00:39:33 +0545

    tailwind config with cfont

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        main: ["Orbitron", "sans-serif"],
+        secondary: ["Monsterrat", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
