a735761fb53254fbc2a551bdb836b6d61e21753a
tyzrex <sulavbaral58@gmail.com>
2024-05-30 22:42:15 +0545

    highlights section

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import Navbar from "./components/global/navbar";
 import About from "./components/sections/about.section";
 import HeroSection from "./components/sections/hero-section";
+import { HighlightsSection } from "./components/sections/highlights-section";
 import ThemeSection from "./components/sections/theme-section";
 import MaxWidthWrapper from "./components/wrapper/maxwidth-wrapper";
+
+function generateRandomColor() {
+  const choices = [
+    "bg-red-400",
+    "bg-green-400",
+    "bg-blue-400",
+    "bg-yellow-400",
+    "bg-indigo-400",
+    "bg-purple-400",
+    "bg-pink-400",
+  ];
+  return choices[Math.floor(Math.random() * choices.length)];
+}
 
 function App() {
   return (
@@ -15,6 +29,14 @@ function App() {
       <MaxWidthWrapper>
         <ThemeSection />
       </MaxWidthWrapper>
+      <HighlightsSection
+        products={Array.from({ length: 4 }, (_, i) => ({
+          title: "test",
+          background: generateRandomColor(),
+          description:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        }))}
+      />
     </>
   );
 }
---
+++ src/components/sections/highlights-section.tsx
@@ -0,0 +1,139 @@
+import React from "react";
+import {
+  motion,
+  useScroll,
+  useTransform,
+  useSpring,
+  MotionValue,
+} from "framer-motion";
+import SectionHeader from "../typography/section-header";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import MaxWidthWrapper from "../wrapper/maxwidth-wrapper";
+import { cn } from "@/lib/utils";
+
+export const HighlightsSection = ({
+  products,
+}: {
+  products: {
+    title: string;
+    description: string;
+    background: string;
+  }[];
+}) => {
+  console.log(products);
+  const firstRow = products;
+  const secondRow = products;
+  const ref = React.useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+
+  const springConfig = { stiffness: 300, damping: 30, bounce: 100 };
+
+  const translateX = useSpring(
+    useTransform(scrollYProgress, [0, 1], [0, 1000]),
+    springConfig
+  );
+  const translateXReverse = useSpring(
+    useTransform(scrollYProgress, [0, 1], [0, -2000]),
+    springConfig
+  );
+  const rotateX = useSpring(
+    useTransform(scrollYProgress, [0, 0.2], [0, 0]),
+    springConfig
+  );
+  const opacity = useSpring(
+    useTransform(scrollYProgress, [0, 0.2], [0.2, 1]),
+    springConfig
+  );
+  const rotateZ = useSpring(
+    useTransform(scrollYProgress, [0, 0.2], [15, 0]),
+    springConfig
+  );
+  const translateY = useSpring(
+    useTransform(scrollYProgress, [0, 0.2], [0, 500]),
+    springConfig
+  );
+  return (
+    <div>
+      <MaxWidthWrapper>
+        <div className="flex items-center w-full justify-center">
+          <SectionHeader headingText="HIGHLIGHTS" />
+        </div>
+      </MaxWidthWrapper>
+      <div
+        ref={ref}
+        className="h-[200vh] py-40 overflow-hidden antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]"
+      >
+        <motion.div
+          style={{
+            rotateX,
+            rotateZ,
+            translateY,
+            opacity,
+          }}
+          className=""
+        >
+          <motion.div className="flex flex-row-reverse space-x-reverse space-x-20 mb-20">
+            {firstRow.map((product) => (
+              <ProductCard
+                product={product}
+                translate={translateX}
+                key={product.title}
+              />
+            ))}
+          </motion.div>
+          <motion.div className="flex flex-row  mb-20 space-x-20 ">
+            {secondRow.map((product) => (
+              <ProductCard
+                product={product}
+                translate={translateXReverse}
+                key={product.title}
+              />
+            ))}
+          </motion.div>
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+const ProductCard = ({
+  product,
+  translate,
+}: {
+  product: {
+    title: string;
+    description: string;
+    background: string;
+  };
+  translate: MotionValue<number>;
+}) => {
+  return (
+    <motion.div
+      style={{
+        x: translate,
+      }}
+      whileHover={{
+        y: -20,
+      }}
+      key={product.title}
+      className="group/product h-auto w-auto relative flex-shrink-0"
+    >
+      <Card
+        className={cn(
+          "px-[24px] pt-[24px] pb-[127px] w-[530px] rounded-none border-none h-[500px] text-white",
+          product.background
+        )}
+      >
+        <CardHeader>
+          <CardTitle className="text-center">{product.title}</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p className="text-center">{product.description}</p>
+        </CardContent>
+      </Card>
+    </motion.div>
+  );
+};
