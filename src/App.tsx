4f76708cf17d7b39a2f204040a4f0839b0b9fc48
tyzrex <sulavbaral58@gmail.com>
2024-05-31 13:21:27 +0545

    add section and styling modifications

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import Navbar from "./components/global/navbar";
 import About from "./components/sections/about.section";
 import HeroSection from "./components/sections/hero-section";
 import { HighlightsSection } from "./components/sections/highlights-section";
+import MentorsSection from "./components/sections/mentors-section";
+import SpeakersSection from "./components/sections/speakers-section";
 import ThemeSection from "./components/sections/theme-section";
 import MaxWidthWrapper from "./components/wrapper/maxwidth-wrapper";
 
@@ -37,6 +39,9 @@ function App() {
             "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
         }))}
       />
+      <SpeakersSection />
+
+      <MentorsSection />
     </>
   );
 }
