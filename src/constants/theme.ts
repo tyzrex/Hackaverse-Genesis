b5ec22f2ccc76e25c3de6c49516d97ecd64479a3
tyzrex <sulavbaral58@gmail.com>
2024-06-06 22:33:13 +0545

    theme description

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -3,26 +3,26 @@ import CardImage from "@/assets/card.jpeg";
 export const themeData = [
   {
     id: 1,
+    themeName: "Good Health and Well-Being",
+    sdGoalNumber: "3",
+    image: CardImage,
+  },
+  {
+    id: 2,
+    themeName: "Quality Education",
+    sdGoalNumber: "4",
+    image: CardImage,
+  },
+  {
+    id: 3,
     themeName: "Clean Water and Sanitation",
     sdGoalNumber: "6",
     image: CardImage,
   },
   {
-    id: 2,
-    themeName: "Climate Action",
-    sdGoalNumber: "13",
-    image: CardImage,
-  },
-  {
-    id: 3,
-    themeName: "Life on Land",
-    sdGoalNumber: "15",
-    image: CardImage,
-  },
-  {
     id: 4,
-    themeName: "Life Below Water",
-    sdGoalNumber: "14",
+    themeName: "Sustainable Cities and Communities",
+    sdGoalNumber: "11",
     image: CardImage,
   },
 ];
